98b9e3a8fa35b9396469261389bd3112a4194e4d
bludy0 <yunuseaslan427@gmail.com>
2024-05-11 05:39:05 +0300

    sfx2

--- AnkuJam/Assets/_Scripts/SoundManager.cs
+++ AnkuJam/Assets/_Scripts/SoundManager.cs
@@ -25,9 +25,27 @@ public class SoundManager : MonoBehaviour
 
     public enum Sounds 
     {
-        EnemyExplosion = 0,
-    
-    
+        tossing = 0,
+        throwingBomb = 1,
+        scenePassC5 = 2,
+        scenePassa4 = 3,
+        playerDeath = 4,
+        menuOpen = 5,
+        menuClose = 6,
+        menuButtonSound3 = 7,
+        menuButtonSound2 = 8,
+        menuButtonSound1 = 9,
+        mainTheme = 10,
+        Gunshot = 11,
+        gillWater = 12,
+        gill = 13,
+        explosionJellyfish = 14,
+        explosion = 15,
+        deathMusic = 16,
+        BossFight_2Opening = 17,
+        Bossfight_2mid = 18,
+        Bossfight_2end = 19,
+
     }
 
     
